b90a9e705e0462ccdda80f30a1b758ae4796dad4
Gopi Kishan <kishan.gopi14.gk@gmail.com>
2018-07-24 17:21:39 +0530

    canvas drawing

--- main.js
+++ main.js
@@ -12,3 +12,88 @@ function random(min,max) {
   var num = Math.floor(Math.random()*(max-min)) + min;
   return num;
 }
+
+//obect model of balls
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size =size;  
+}
+//draw balls on canvas
+Ball.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+  ctx.fill();
+}
+
+//updates states of ball and bouncing of the walls
+Ball.prototype.update = function() {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+  // alert("hi");
+  this.x += this.velX;
+  this.y += this.velY;
+}
+
+//Add collision detection
+Ball.prototype.collisionDetect = function() {
+  for (var j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      var dx = this.x - balls[j].x;
+      var dy = this.y - balls[j].y;
+      var distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+      }
+    }
+  }
+}
+
+
+var balls = [];
+//Animates balls
+function loop(){
+  ctx.fillStyle = 'rgb(0,0,0, 0.25)';
+  ctx.fillRect(0, 0, width , height);
+
+  while(balls.length < 25) {
+    var size = random(10 , 20);
+    var ball = new Ball(
+      random(0+size , width - size),
+      random(0+size , height-size),
+      random(-7, 7),
+      random(-7 , 7),
+      'rgb(' + random(0, 255) + ','+ random(0, 255) + ',' + random(0, 255) +')',
+      size
+    );
+
+    balls.push(ball);
+    }
+
+    for(var i =0 ; i<balls.length; i++) {
+      balls[i].draw();
+      balls[i].update();
+      balls[i].collisionDetect();
+    }
+    requestAnimationFrame(loop);
+}
+
+loop();
